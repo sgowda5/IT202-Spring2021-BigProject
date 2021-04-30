1002907a052bf05eec652b85290a06a56447a966
Surya Gowda <sgowda5@uic.edu>
2021-04-30 08:33:32 +0000

    added project code

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = "Project"
+const assets = [
+  "./",
+  "./index.html",
+  "./icons/policeHat.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
